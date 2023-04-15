b62afd1e4a8f4ee891ba335b653d15a4483ad483
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-04-15 04:37:51 +0000

    c

--- =title.py
+++ =title.py
@@ -3,9 +3,10 @@ import subprocess
 tdata = []
 data = []
 # git ls-tree -r --name-only HEAD > -title.txt
-out = subprocess.run(["git","ls-tree","-r","--name-only","HEAD",">","-title.txt"])
-with open("-title.txt","r+") as f:
+out = subprocess.run(["git","ls-tree","-r","--name-only","HEAD",">","+title.txt"])
+with open("+title.txt","r+") as f:
     tdata=f.readlines()
+    print(tdata)
     for i in range(len(tdata)):
         if tdata[i] in "mp3":
             data.append(tdata[i])
