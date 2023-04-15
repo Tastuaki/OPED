b41a6cf735b254df44a0ddf6aedfba22c6c36eeb
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-04-15 04:46:27 +0000

    c

--- =title.py
+++ =title.py
@@ -4,11 +4,11 @@ tdata = []
 data = []
 # git ls-tree -r --name-only HEAD > -title.txt
 out = subprocess.run(["git","ls-tree","-r","--name-only","HEAD",">","+title.txt"])
-with open("+title.txt","r+") as f:
-    tdata=f.readlines()
-    print(tdata)
-    for i in range(len(tdata)):
-        if "mp3" in tdata[i]:
-            data.append(tdata[i])
-    print(data)
-    f.writelines(data)
+# with open("+title.txt","r+") as f:
+#     tdata=f.readlines()
+#     for i in range(len(tdata)):
+#         if "mp3" in tdata[i]:
+#             data.append(tdata[i])
+#     f.truncate(0)
+#     print(data)
+#     f.writelines(data)
