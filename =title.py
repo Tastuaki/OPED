28f49a698c2b5ac355caf44dd99fe69d601ce1b0
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-05-15 01:17:20 +0900

    c

--- =title.py
+++ =title.py
@@ -7,8 +7,13 @@ con = ""
 # git ls-tree -r --name-only HEAD > +title.txt
 path = os.getcwd()
 print(path)
-out = subprocess.run(["dir","*.mp3","/b",">","+title.txt"],shell=True)
+out = subprocess.run(["dir","*.mp3","/b",">","++title.txt"],shell=True)
 # out = subprocess.run(["git","ls-tree","-r","--name-only","HEAD",">","+title.txt"],shell=True)
+with codecs.open("++title.txt", "r", "shift_jis") as s:
+    data = s.readlines()
+    with codecs.open("+title.txt", "w", "utf-8") as u:
+        for row in data:
+            u.write(row)
 # with open("+title.txt","r+",encoding="utf-8") as f:
 #     tdata=f.readlines()
 #     f.truncate(0)
