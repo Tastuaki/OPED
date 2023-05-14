ee37c19973319d121c6c9db262a7ac4860aeee95
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-05-15 00:49:40 +0900

    c

--- =title.py
+++ =title.py
@@ -1,17 +1,18 @@
 import subprocess
 import os
+import codecs
 
 tdata = []
 con = ""
 # git ls-tree -r --name-only HEAD > +title.txt
 path = os.getcwd()
 print(path)
-out = subprocess.run(["dir","/b",">","+title.txt"],shell=True)
+out = subprocess.run(["dir","*.mp3","/b",">","+title.txt"],shell=True)
 # out = subprocess.run(["git","ls-tree","-r","--name-only","HEAD",">","+title.txt"],shell=True)
-with open("+title.txt","r+") as f:
-    tdata=f.readlines()
-    f.truncate(0)
-    f.seek(0)
-    for con in tdata:
-        if "mp3" in con:
-            f.write(con.replace("\'",""))
+# with open("+title.txt","r+",encoding="utf-8") as f:
+#     tdata=f.readlines()
+#     f.truncate(0)
+#     f.seek(0)
+#     for con in tdata:
+#         if "mp3" in con:
+#             f.write(con.replace("\'",""))
