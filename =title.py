5afb761d3ffbb5dab00812a408bb8855bd832914
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-06-19 15:30:50 +0900

    c

--- =title.py
+++ =title.py
@@ -1,35 +1,80 @@
-import subprocess
 import os
-import codecs
-import io
 import glob
 from itertools import count
 
 tdata = []
-# con = ""
+sdata = [[],[],[],[],[],[]]
+sym = ["前奏曲","OP","ED","劇中曲","挿入歌","主題歌"]
+i = 0
+j = 0
+k = 0
+lb = 0
+
+def ex(s,data,p):
+    l = 0
+    od = []
+    l = len(data)
+    for i in range(p,l):
+        if s in data[i]:
+            od.append(data[i])
+    return od
 
 base = os.getcwd()
 files_path = glob.glob(os.path.join(base,"*.mp3"))
 files = [os.path.basename(i) for i in files_path]
+max = len(files)
 
 with open("+title.txt",'w+', encoding='utf-8') as f:
-    for i in len(files):
-        tdata = []
-        for j in count(i):
-            if("〜" in files[j]):
-                orow = files[j]
-                files[j] = files[j].replace("〜","～")
-                os.rename(base+"\\"+orow,base+"\\"+files[i])
-            files[j] += "\n"
-            tdata.append(files[j])
-            b = files[j][:files[j].find("(")]
-            b = b[:b.rfind(" ")]
-            a = files[j+1][:files[j+1].find("(")]
-            a = a[:a.rfind(" ")]
-            print("a: "+ a,end="|")
-            print("b: "+ b,end="|\n")
-            if(a != b):
-                i = j
-                break
-        tdata.reverse()
-        f.writelines(tdata)
+    for i in range(max):
+        if i == j:
+            print(str(i)+":"+str(j))
+            bdata = []
+            for j in count(i):
+                if j < max - 1:
+                    if "〜" in files[j]:
+                        orow = files[j]
+                        files[j] = files[j].replace("〜","～")
+                        os.rename(base+"\\"+orow,base+"\\"+files[i])
+                    files[j] += "\n"
+                    bdata.append(files[j])
+                    b = files[j][:files[j].find("(")]
+                    b = b[:b.rfind(" ")]
+                    a = files[j+1][:files[j+1].find("(")]
+                    a = a[:a.rfind(" ")]
+                    if a != b:
+                        j += 1
+                        break
+                else:
+                    break
+            print(bdata)
+            lb = len(bdata)
+            if lb != 1:
+                k = 0
+                while(k < lb):
+                    print(str(k)+":"+bdata[k])
+                    for s in sym:
+                        if s in bdata[k]:
+                            if k+1 < lb:
+                                if s in bdata[k+1]:
+                                    sdata[sym.index(s)] = ex(s,bdata,k)
+                                    k += len(sdata[sym.index(s)]) - 1
+                                else:
+                                    sdata[sym.index(s)] = [bdata[k]]
+                            else:
+                                sdata[sym.index(s)] = [bdata[k]]
+                    k += 1
+            else:
+                for s in sym:
+                    if s in bdata[0]:
+                        sdata[sym.index(s)] = [bdata[0]]
+                        break
+            # print(sdata)
+            for s in sym:
+                for l in range(len(sdata)):
+                    if sdata[l]:
+                        if s in sdata[l][0]:
+                            tdata.extend(sdata[l])
+            sdata = [[],[],[],[],[],[]]
+            # print(tdata)
+            f.writelines(tdata)
+            tdata = []
