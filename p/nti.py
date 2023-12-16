d5315943e3ad29c6363fe3e917f7e3fd2085ab9a
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-12-17 01:15:51 +0900

    new

--- p/nti.py
+++ p/nti.py
@@ -32,7 +32,7 @@ files = [os.path.basename(i) for i in files_path]
 max = len(files)
 # print(max)
 
-with open("new",'w+', encoding='utf-8') as f:
+with open(base+"\\new",'w+', encoding='utf-8') as f:
     for i in range(max):
         if i == j:
             # print(str(i)+":"+str(j))
